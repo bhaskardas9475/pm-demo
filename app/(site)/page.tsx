0cf88f361a8ef5d1256070f4bfb55f711b62d42d
Bhaskar Das <bhaskardas@Bhaskars-Air.lan>
2024-11-16 14:21:02 +0530

    Metadata changes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,8 +14,8 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
-  description: "This is Home for Solid Pro",
+  title: "Property Matrimony",
+  description: "At Property Matrimony, we are dedicated to transforming the real estate experience for our clients As a premier real estate platform, we specialize in offering a wide range of premium plots, villas, and apartments, tailored to meet the diverse needs of our discerning clientele. Backed by a committed team of professionals, we leverage advanced technology and comprehensive market insights to guide our clients through every step of their property journey—from the initial search to the final possession.",
   // other metadata
 };
 
--- app/(site)/privacy-policy/page.tsx
+++ app/(site)/privacy-policy/page.tsx
@@ -3,10 +3,10 @@ import PageHeader from "@/components/PageHeader";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Docs Page - Solid SaaS Boilerplate",
-    description: "This is Docs page for Solid Pro",
+    title: "Property Matrimony",
+    description: "At Property Matrimony, we are dedicated to transforming the real estate experience for our clients As a premier real estate platform, we specialize in offering a wide range of premium plots, villas, and apartments, tailored to meet the diverse needs of our discerning clientele. Backed by a committed team of professionals, we leverage advanced technology and comprehensive market insights to guide our clients through every step of their property journey—from the initial search to the final possession.",
     // other metadata
-};
+  };
 
 export default function PrivacyPolicyPage() {
     const items = [
